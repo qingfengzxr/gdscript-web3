2227ac8b9572c12511dfd4c283602275321f80bc
zxr <xiaoyao1441546669@163.com>
2024-05-21 15:10:04 +0800

    use c++ modules to implement secp256k1 tools

---
+++ modules/secp256k1_wrapper/register_types.h
@@ -0,0 +1,7 @@
+/* register_types.h */
+
+#include "modules/register_module_types.h"
+
+void initialize_secp256k1_wrapper_module(ModuleInitializationLevel p_level);
+void uninitialize_secp256k1_wrapper_module(ModuleInitializationLevel p_level);
+/* yes, the word in the middle must be the same as the module folder name */
