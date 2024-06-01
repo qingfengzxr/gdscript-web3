abb2cf16f2e4925243bd358a7434544c251aea5a
cooper <xiaoyao1441546669@163.com>
2024-06-01 22:45:54 +0800

    replace the current keccak library with the keccak implementation from the keccak team
    * according to the documentation of the keccak team, the performance is even better
    * use libethc library base to eth protocol library(rlp, checksumaddress, hex)

--- modules/web3/keccak_wrapper.cpp
+++ modules/web3/keccak_wrapper.cpp
@@ -1,6 +1,9 @@
 #include "keccak_wrapper.h"
-#include "keccak.h"
 
+#define KECCAK256_RATE 1088
+#define KECCAK256_CAPACITY 512
+#define KECCAK256_HASHBITLEN 256
+#define KECCAK256_DELIMITED_SUFFIX 0x1
 
 KeccakWrapper::KeccakWrapper() {
 	;
@@ -11,14 +14,14 @@ KeccakWrapper::~KeccakWrapper() {
 }
 
 PackedByteArray KeccakWrapper::keccak256_hash(const PackedByteArray &data) {
-	union ethash_hash256 hash;
+	uint8_t hash[32];
 
 	const uint8_t* data_ptr = data.ptr();
-	hash = ethash_keccak256(data_ptr, data.size());
+	eth_keccak256(hash, data_ptr, data.size());
 
 	PackedByteArray result;
 	result.resize(32);
-	memcpy(result.ptrw(), hash.bytes, 32);
+	memcpy(result.ptrw(), hash, 32);
 	return result;
 }
 
